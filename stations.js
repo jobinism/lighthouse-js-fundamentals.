bbfe4c16df9aa538f6e1f58bbd7735d63bd3385e
Jarret Coyle <jarretcoyletoo@gmail.com>
2022-07-24 09:50:43 -0700

    won't print the name.

---
+++ stations.js
@@ -0,0 +1,19 @@
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+  ['Blue Mountain State'], 60, 'school'
+];
+
+function chooseStations(stations) {
+  for(const station of stations){
+    const capacity = station[1];
+    const type = station[2];
+    if (capacity >= 20) {
+      if (type === "school") {
+        console.log(station[0]);
+      }
+    }
+    
+  }
+}

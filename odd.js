805331715429679e1b829c473755df25142ca3a3
Jarret Coyle <jarretcoyletoo@gmail.com>
2022-07-23 20:06:58 -0700

    Odd Function Completion

---
+++ odd.js
@@ -0,0 +1,10 @@
+const isOdd = function(num){
+  return num % 2 != 0
+
+}
+
+const twentyThreeIsOdd = isOdd(23);
+const fortyFourIsOdd = isOdd(44);
+
+console.log(twentyThreeIsOdd);
+console.log(fortyFourIsOdd);

b4071affe063b1f246f26a3cdd1207fc59aaa84b
Jarret Coyle <jarretcoyletoo@gmail.com>
2022-07-25 21:40:48 -0700

    wowowowowow

--- agecalc.js
+++ agecalc.js
@@ -3,6 +3,6 @@ function ageCalculator(name, yearOfBirth, currentYear) {
     return name + " is " + age + " years old. "
   }
   
-  console.log(ageCalculator(“Suzie”, 1983, 2015));
-  console.log(ageCalculator(“Jack”, 2003, 2015));
-  console.log(ageCalculator(“Ali”, 2015, 2015));
+  console.log(ageCalculator('Suzie', 1983, 2015));
+  console.log(ageCalculator('Jack', 2003, 2015));
+  console.log(ageCalculator('Ali', 2015, 2015));

bdd6f76e55855e6426748115d7438002e4f24113
Jarret Coyle <jarretcoyletoo@gmail.com>
2022-07-28 12:53:18 -0700

    keep em coming

---
+++ looping-array.js
@@ -0,0 +1,9 @@
+console.log("Items in your pack:")
+
+let inventory = ['Watermelon', 'Knife', 'Bread', 'Pumpkin Seeds', 'Belt Buckle', 'Half Bottle of Beer', 'Dandelion'
+]
+let i = 0;
+while (i < inventory.length) {
+  console.log(inventory[i]);
+  i++;
+}
